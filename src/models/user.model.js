7bd62351c288b144b39dba9a7b5b0acc01235ebf
Fathma <siddiquefathma@gmail.com>
2019-10-16 17:02:56 +0600

    changed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,11 +3,12 @@ const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
     name: { type: String },
-    email: { type:String, required:true},
+    email: { type:String, required:true },
     password: { type: String, required: true },
-    role:{type: String, required:true},
-    imageURL:{type: String, required:true},
-    date:{type: String, default:Date.now}
+    role: { type: String, required:true },
+    imageURL: { type: String, required:true },
+    verified: { type: Boolean, default:false },
+    date: { type: String, default:Date.now }
 })
 
 module.exports = mongoose.model('User', UserSchema, 'users')
