497eba79f6ef5d4a22106e159ca0baef00c92855
Fathma <siddiquefathma@gmail.com>
2019-10-16 17:00:47 +0600

    image(not tested)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     email: { type:String, required:true},
     password: { type: String, required: true },
     role:{type: String, required:true},
+    imageURL:{type: String, required:true},
     date:{type: String, default:Date.now}
 })
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const passport = require('passport')
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
 const router = express.Router()
 
 const user = require('../controllers/user.controller')
 const validate = require('../helpers/validations')
+const keys = require('../../config/keys')
 
-router.post('/register', validate.UserRegistration, user.register)
+var filename;
+
+// create storage engine
+const storage = new GridFsStorage(
+  {
+    url: keys.database.mongoURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) return reject(err);
+          
+          filename = buf.toString('hex') + path.extname(file.originalname)
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'fs'
+          };
+          resolve(fileInfo)
+        });
+      });
+    }
+  });
+
+const upload = multer({ storage })
+
+
+router.post('/register', validate.UserRegistration, upload.single('imageURL'), user.register)
 router.post('/login', validate.UserLogin, passport.authenticate('local', { session: false }), user.login)
 
 
